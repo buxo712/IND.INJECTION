251f31bd1dfa1df57fea914cfe59e9667c8ad9fe
buxo712 <bwenge45@gmail.com>
2025-04-02 00:24:07 -0700

    commit#6
    in the NV, I created a method  of specific kind of classical music that was implementing in the interface class

--- src/ClassicalMusicService.java
+++ src/ClassicalMusicService.java
@@ -1,12 +1,15 @@
 public class ClassicalMusicService implements MusicService  {
     // Implementation of MusicService for playing classical music.
-    public void playMusic() { // Logic for playing classical music.
+    public void playMusic() {
+        +"2.Bebop \n"
+                +"3.Cool jazz \n"// Logic for playing classical music.
         System.out.println("Playing classical music...\n"// specific kind of classical music printed
                            +"1.Baroquen\n"
                            +"2.Romantic\n"
                            +"3.Classical Period\n"
                            +"4.Impressionism\n"
                             +"5.Moderm\n");
+
     }
 }
 
